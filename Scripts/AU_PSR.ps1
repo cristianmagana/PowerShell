9b0128daa20591e3a16a540823486ebd8d969b95
engineeredkaos <33166233+engineeredkaos@users.noreply.github.com>
2018-07-10 16:39:00 -0700

    Delete AU_PSR.ps1

--- Scripts/AU_PSR.ps1
+++
@@ -1,7 +0,0 @@
-﻿get-service winrm
-Enable-PSRemoting –force
-winrm quickconfig
-
-New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name WindowsUpdate
-New-Item HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name AU
-New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU -Name NoAutoUpdate -Value 1
